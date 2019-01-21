efadfa203202f044b1583b44ad2f349b1324d7e6
Omar01 <omarb28@gmail.com>
2019-01-21 17:30:57 +0300

    add ultrasound sensor

---
+++ ultrasonic.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO
+import time
+
+try:
+
+      GPIO.setmode(GPIO.BCM)
+
+      PIN_TRIGGER = 4
+      PIN_ECHO = 17
+
+      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+      GPIO.setup(PIN_ECHO, GPIO.IN)
+
+      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+      print "Waiting for sensor to settle"
+
+      time.sleep(2)
+
+      print "Calculating distance"
+      
+      while True:
+	      time.sleep(0.75)
+
+	      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+	
+	      time.sleep(0.00001)
+	
+	      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+	
+	      while GPIO.input(PIN_ECHO)==0:
+	            pulse_start_time = time.time()
+	      while GPIO.input(PIN_ECHO)==1:
+	            pulse_end_time = time.time()
+	
+	      pulse_duration = pulse_end_time - pulse_start_time
+	      distance = round(pulse_duration * 17150, 2)
+	      
+
+	      print "Distance:",distance,"cm"
+
+              if distance <= 10.0:
+		     print("There is an object closeby.");
+
+finally:
+      GPIO.cleanup()                     
