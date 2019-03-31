f448f02484794a17385dd57acc6f4b5372c75a5f
Omar01 <omarb28@gmail.com>
2019-03-31 15:40:54 +0300

    read from RFID and display

--- RFID/Read.py
+++ RFID/Read.py
@@ -1,7 +1,17 @@
 import time
 import RPi.GPIO as GPIO
 import MFRC522
- 
+
+import paho.mqtt.client as paho
+client= paho.Client("RFID-Client")
+client.subscribe("RFID/cardID")#subscribe
+time.sleep(2)
+
+print("connecting to broker ","localhost")
+client.connect("192.168.1.192", 1883, 0)#connect
+print("subscribing ")
+client.subscribe("RFID/cardID")#subscribe
+
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
  
@@ -24,9 +34,14 @@ try:
     if status == MIFAREReader.MI_OK:
  
       # Print UID
-      print("UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
+      #UID = str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+      UID = "{0:x} {1:x} {2:x} {3:x}".format(uid[0], uid[1], uid[2], uid[3]).upper()
+
+      print("UID: ", UID)
+      client.publish("RFID/cardID", UID)#publish
  
       time.sleep(2)
  
 except KeyboardInterrupt:
+  client.disconnect() #disconnect
   GPIO.cleanup()
